30bd26dd68116e6090e938aa20c5582389fdeeb9
CEULuisFelipeRiera <159237065+CEULuisFelipeRiera@users.noreply.github.com>
2024-02-29 09:42:06 +0000

    creacionDEpaquetes

---
+++ Caso_Final/casofinalSImaven/src/main/java/casofinaltema2/App.java
@@ -0,0 +1,13 @@
+package casofinaltema2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
